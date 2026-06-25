cecaca506db2c590e850a731112767f14b605ea5
João Vitor <joaovitorfesouza@gmail.com>
2026-06-25 05:52:33 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+print('Stup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600,400))
+print('Stup End')
+
+print('Loop Start')
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Close Window
+            quit() # end pygame
